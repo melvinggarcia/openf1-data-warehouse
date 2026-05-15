0bc6f9d05d57a13678e86a634a58bb9e03b70528
melvinggarcia <244387045+melvinggarcia@users.noreply.github.com>
2026-05-15 09:59:47 -0400

    Delete models directory
    Delete temporary location of models directory

--- models/fact_position.sql
+++
@@ -1,9 +0,0 @@
--- fact_position.sql 
-SELECT 
-     session_key,
-     driver_number,
-     position,
-     CAST(date as DATETIME) AS date,
-FROM {{ source('raw', 'raw_position') }} 
-WHERE driver_number IS NOT NULL
-    AND session_key IS NOT NULL
--- models/fact_starting_grid.sql
+++
@@ -1,9 +0,0 @@
--- fact_starting_grid.sql 
-SELECT 
-     session_key,
-     driver_number,
-     position,
-     lap_duration,
-FROM {{ source('raw', 'raw_starting_grid') }} 
-WHERE driver_number IS NOT NULL
-    AND session_key IS NOT NULL
